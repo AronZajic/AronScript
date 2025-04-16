413d3202d0e7951c74c61c5cf17296a70d45ab0e
Aron Zajic <hollyaron@gmail.com>
2025-04-16 23:15:07 +0200

    Added tests for decimal types

--- tests.c
+++ tests.c
@@ -6,7 +6,7 @@
 GHashTable *variables;
 GHashTable *functions;
 
-static void test(void **state){
+static void test_integer(void **state){
 
     (void) state;
 
@@ -142,9 +142,146 @@ static void test(void **state){
     free(context);
 }
 
+static void test_decimal(void **state){
+
+    (void) state;
+
+	variables = g_hash_table_new_full(g_str_hash, g_str_equal, g_free, free);
+	functions = g_hash_table_new_full(g_str_hash, g_str_equal, g_free, freeFunctionDeclaration);
+
+	struct Context *context = malloc(sizeof(struct Context));
+	context->parent = NULL;
+	context->variables = variables;
+	context->functions = functions;
+
+    char* s = "1.0";
+    struct Node *n = parse(s);
+    struct EvalNode result;
+    result = eval(n, context);
+
+    assert_float_equal(result.value.decimalValue, 1.0, 0.1);
+
+    s = "-1.0";
+    n = parse(s);
+    result = eval(n, context);
+
+    assert_float_equal(result.value.decimalValue, -1.0, 0.1);
+
+    s = "+1.0";
+    n = parse(s);
+    result = eval(n, context);
+
+    assert_float_equal(result.value.decimalValue, 1.0, 0.1);
+
+    s = "1.0+1.0";
+    n = parse(s);
+    result = eval(n, context);
+
+    assert_float_equal(result.value.decimalValue, 1.0+1.0, 0.1);
+
+    s = "(1.0+1.0)";
+    n = parse(s);
+    result = eval(n, context);
+
+    assert_float_equal(result.value.decimalValue, (1.0+1.0), 0.1);
+
+    s = "3.0+3.0+3.0+5.0*5.0*2.0+1.0+2.0+2.0";
+    n = parse(s);
+    result = eval(n, context);
+
+    assert_float_equal(result.value.decimalValue, 3.0+3.0+3.0+5.0*5.0*2.0+1.0+2.0+2.0, 0.1);
+
+    s = "5.0*5.0*2.0+2.0+1.0+1.0+2.0*2.0*2.0*2.0";
+    n = parse(s);
+    result = eval(n, context);
+
+    assert_float_equal(result.value.decimalValue, 5.0*5.0*2.0+2.0+1.0+1.0+2.0*2.0*2.0*2.0, 0.1);
+
+    s = "5.0*(5.0+6.0*(9.0-7.0)/3.0)";
+    n = parse(s);
+    result = eval(n, context);
+
+    assert_float_equal(result.value.decimalValue, 5.0*(5.0+6.0*(9.0-7.0)/3.0), 0.1);
+
+    s = "123.0+300.0";
+    n = parse(s);
+    result = eval(n, context);
+
+    assert_float_equal(result.value.decimalValue, 123.0+300.0, 0.1);
+
+    s = "5.0  *      (     5.0     +    6.0    * ( 9.0   - 7.0 ) / 3.0 )";
+    n = parse(s);
+    result = eval(n, context);
+
+    assert_float_equal(result.value.decimalValue, 5.0  *      (     5.0     +    6.0    * ( 9.0   - 7.0 ) / 3.0 ), 0.1);
+
+    s = "123.0      +         300.0     ";
+    n = parse(s);
+    result = eval(n, context);
+
+    assert_float_equal(result.value.decimalValue, 123.0      +         300.0     , 0.1);
+
+    s = "2.0>1.0";
+    n = parse(s);
+    result = eval(n, context);
+
+    assert_int_equal(result.value.integerValue, 1);
+
+    s = "2.0<1.0";
+    n = parse(s);
+    result = eval(n, context);
+
+    assert_int_equal(result.value.integerValue, 0);
+
+    s = "(2.0+2.0)>3.0";
+    n = parse(s);
+    result = eval(n, context);
+
+    assert_int_equal(result.value.integerValue, 1);
+
+    s = "2.0+2.0>3.0";
+    n = parse(s);
+    result = eval(n, context);
+
+    assert_int_equal(result.value.integerValue, 1);
+
+    s = "2.0*2.0>3.0";
+    n = parse(s);
+    result = eval(n, context);
+
+    assert_int_equal(result.value.integerValue, 1);
+
+    s = "Decimal a = 567.0";
+    n = parse(s);
+    eval(n, context);
+    s = "a";
+    n = parse(s);
+    result = eval(n, context);
+
+    assert_float_equal(result.value.decimalValue, 567.0, 0.1);
+
+    s = "Decimal abc = 567.0";
+    n = parse(s);
+    eval(n, context);
+    s = "abc=5.0";
+    n = parse(s);
+    eval(n, context);
+    s = "abc";
+    n = parse(s);
+    result = eval(n, context);
+
+    assert_float_equal(result.value.decimalValue, 5.0, 0.1);
+
+    g_hash_table_destroy(variables);
+    g_hash_table_destroy(functions);
+
+    free(context);
+}
+
 int main(void) {
     const struct CMUnitTest tests[] = {
-        cmocka_unit_test(test),
+        cmocka_unit_test(test_integer),
+        cmocka_unit_test(test_decimal),
     };
 
     return cmocka_run_group_tests(tests, NULL, NULL);
